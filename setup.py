49f7ac20b60e451802c8eafa5bd49fdbf7804704
julo4153 <joachim.thiemann@uni-oldenburg.de>
2017-05-07 01:09:39 +0200

    Adding requirements of scipy>0.18 and numpy.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
 setup(name='nnresample',
-      version='0.99',
-      description='A resampling function based on placing the filters first null on Nyquist (Null-on-Nyquist Resample)',
+      version='0.1',
+      description='A resampling function based on placing the first null on Nyquist (Null-on-Nyquist Resample)',
       url='https://github.com/jthiem/nnresample',
       author='Joachim Thiemann',
       author_email='Joachim.Thiemann@gmail.com',
       license='CC-BY 3.0',
       py_modules=['nnresample'],
+      install_reuires=[
+          'scipy>=0.18.0',
+          'numpy'
+      ],
       )
