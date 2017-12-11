c39db3d6f9103019cbf27ef79a8622e131ad325f
Joachim Thiemann (on Flip) <joachim.thiemann@uni-oldenburg.de>
2017-12-11 10:41:25 +0100

    Adding test signals.

--- test_nnresample.py
+++ test_nnresample.py
@@ -1,6 +1,16 @@
 from nnresample import compute_filt, resample
 from disambiguate import beta_from_As, As_from_beta, disambiguate_params
-from numpy import isclose
+from numpy import cumsum, exp, log, sin, logspace, linspace, isclose, zeros, pi
+
+# test data
+t441 = linspace(0, 1, 44100)
+t480 = linspace(0, 1, 48000)
+ifreq441 = exp(linspace(log(100), log(20000), 22050))
+ifreq480 = exp(linspace(log(100), log(20000), 24000))
+refsig441 = zeros(44100)
+refsig480 = zeros(48000)
+refsig441[11025:33075] = sin(cumsum(ifreq441*2*pi/44100))
+refsig480[12000:36000] = sin(cumsum(ifreq480*2*pi/48000))
 
 def test_beta_from_As():
     assert(isclose(beta_from_As(60), 5.65326))
