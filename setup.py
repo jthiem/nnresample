103d4f16e6dcabb1d64e35fb174bd457dc7be842
mpariente <pariente.mnl@gmail.com>
2018-02-09 11:09:24 +0100

    Include nnresample version in setup.py and remove scipy<2.0.0

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup, find_packages
 import nnresample
 
 setup(name='nnresample',
-      version=nnresample.__version__,
+      version=0.2.3,
       description='A resampling function based on placing the first null on Nyquist (Null-on-Nyquist Resample)',
       url='https://github.com/jthiem/nnresample',
       author='Joachim Thiemann',
       author_email='Joachim.Thiemann@gmail.com',
       license='CC-BY 3.0',
       packages=find_packages(include=['nnresample']),
-      install_requires=[
-          'scipy>=0.18.0,<2.0.0'  # NumPy is a dependancy of SciPy
-      ]
+      install_requires=['scipy>=0.18.0']
       )
