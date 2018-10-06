0aa847351707612c4a1d42120aa8e732fc406d74
Stefano Miccoli <stefano.miccoli@polimi.it>
2018-10-07 00:33:54 +0200

    setup for flit build-system
    - create pyproject.toml with metada form old setup.py
    - added version number ("0.2.4.dev0") in nnresample.__init__
    - removed redundant 'from . import utility'
    - added __all__ for better package documentation

--- nnresample/__init__.py
+++ nnresample/__init__.py
@@ -1,2 +1,9 @@
+"""A resampling function based on placing the first null on Nyquist
+   (Null-on-Nyquist Resample)
+"""
+
+__version__ = "0.2.4.dev0"
+
+__all__ = ["resample", "compute_filt"]
+
 from .nnresample import resample, compute_filt
-from . import utility
