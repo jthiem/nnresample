a36d707c735fcfd8d0b4380c57be2d4a06fff608
Joachim Thiemann <Joachim.Thiemann@gmail.com>
2018-10-17 23:44:01 +0200

    Attemting a different way of specifying the version in a single location.

--- nnresample/__init__.py
+++ nnresample/__init__.py
@@ -1,2 +1,4 @@
+__version__ = '0.2.4dev1'
+
 from .nnresample import resample, compute_filt
 from . import utility
--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
+import codecs
+import re
+import os
 from setuptools import setup, find_packages
 
+# read the version information from nnresample/__init__.py
+# This code was taken from https://packaging.python.org/guides/single-sourcing-package-version/
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setup(name='nnresample',
-      version='0.2.4',
+      version=find_version("nnresample", "__init__.py"),
       description='A resampling function based on placing the first null on Nyquist (Null-on-Nyquist Resample)',
       url='https://github.com/jthiem/nnresample',
       author='Joachim Thiemann',
